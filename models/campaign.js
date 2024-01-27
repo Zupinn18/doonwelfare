ef0feaa115fe5eff12bfa51773576216ab1b4c67
shivam malhotra <141710462+Zupinn18@users.noreply.github.com>
2024-01-27 15:24:27 +0530

    U

---
+++ models/campaign.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+// Define the Campaign schema
+const campaignSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+// Create a Campaign model based on the schema
+const Campaign = mongoose.model('Campaign', campaignSchema);
+
+export default Campaign;
